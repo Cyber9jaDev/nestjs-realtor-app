9ffe6bedd762f4d4834990ce53db57147d9f47d2
cyber9ja <ayodejioladapo15@gmail.com>
2024-10-29 16:18:51 +0000

    Add unit tests for HomeService createHome

--- src/home/home.service.spec.ts
+++ src/home/home.service.spec.ts
@@ -18,6 +18,23 @@ const mockGetHomes = [
   },
 ];
 
+const mockCreateHome = {
+  id: 15,
+  address: '2 Realtor Street',
+  city: 'Ontario',
+  price: 5070000,
+  propertyType: PropertyType.RESIDENTIAL,
+  image: 'img1',
+  number_of_bedrooms: 2,
+  number_of_bathrooms: 2,
+  land_size: 1000,
+};
+
+const mockCreateImages = [
+  { id: 1, url: 'src1' },
+  { id: 2, url: 'src2' },
+];
+
 describe('HomeService', () => {
   let homeService: HomeService;
   let databaseService: DatabaseService;
@@ -31,6 +48,10 @@ describe('HomeService', () => {
           useValue: {
             home: {
               findMany: jest.fn().mockReturnValue(mockGetHomes),
+              create: jest.fn().mockReturnValue(mockCreateHome),
+            },
+            image: {
+              createMany: jest.fn().mockReturnValue(mockCreateImages),
             },
           },
         },
@@ -81,6 +102,39 @@ describe('HomeService', () => {
       await expect(homeService.getHomes(filters)).rejects.toThrow(
         NotFoundException,
       );
+    });
+  });
+
+  describe('createHomes', () => {
+    const mockCreateHomesParams = {
+      address: '2 Realtor Street',
+      city: 'Ontario',
+      price: 5070000,
+      propertyType: PropertyType.RESIDENTIAL,
+      images: [{ url: 'img 1' }],
+      numberOfBedrooms: 2,
+      numberOfBathrooms: 2,
+      landSize: 1000,
+    };
+
+    it('should call database home.create with the correct payload', async () => {
+      const mockDatabaseCreateHome = jest.fn().mockReturnValue(mockCreateHome);
+      jest
+        .spyOn(databaseService.home, 'create')
+        .mockImplementation(mockDatabaseCreateHome);
+      await homeService.createHome(5, mockCreateHomesParams);
+      expect(mockDatabaseCreateHome).toHaveBeenCalledWith({
+        data: {
+          address: '2 Realtor Street',
+          city: 'Ontario',
+          price: 5070000,
+          propertyType: PropertyType.RESIDENTIAL,
+          number_of_bedrooms: 2,
+          number_of_bathrooms: 2,
+          land_size: 1000,
+          realtor_id: 5
+        },
+      });
     });
   });
 });

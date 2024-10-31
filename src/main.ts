1bfcf88c99bd154a525be1bf6f428e9691a21fb0
cyber9ja <ayodejioladapo15@gmail.com>
2024-10-31 21:04:36 +0000

    swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
+// Import statements are properly arranged in a logical order
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'; 
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  // App creation
   const app = await NestFactory.create(AppModule);
+
+  // Swagger documentation setup
+  const config = new DocumentBuilder()
+    .setTitle('Realtor App')
+    .setDescription('Realtor APP API description')
+    .setVersion('1.0')
+    .addTag('Realtor')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // Global pipes configuration
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
@@ -12,6 +27,7 @@ async function bootstrap() {
     }
   }));
 
+  // Start server
   await app.listen(3000);
 }
 bootstrap();

e813a7f5e06efbadae16d284c5d0779709ca5250
cyber9ja <ayodejioladapo15@gmail.com>
2024-10-31 10:17:21 +0000

    Add unit tests for HomeController getHomes

--- src/home/home.controller.spec.ts
+++ src/home/home.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HomeController } from './home.controller';
+import { HomeService } from './home.service';
 
 describe('HomeController', () => {
   let controller: HomeController;
+  let homeService: HomeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HomeController],
+      providers: [
+        {
+          provide: HomeService,
+          useValue: {
+            getHomes: () => jest.fn().mockReturnValue([]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<HomeController>(HomeController);
+    homeService = module.get<HomeService>(HomeService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getHomes', () => {
+    it('should construct filter object correctly', async () => {
+      // Create a function that returns an empty array
+      const mockGetHomes = jest.fn().mockReturnValue([]);
+      // Spy on homeService and get the getHomes method
+      jest.spyOn(homeService, 'getHomes').mockImplementation(mockGetHomes);
+      await controller.getHomes('Ibadan', '1000');
+      // getHomes from homeService -:- getHomes(filter: Filter)
+      expect(mockGetHomes).toHaveBeenCalledWith({
+        city: 'Ibadan', //city
+        price: {
+          gte: 1000, // minPrice
+        },
+      });
+    });
   });
 });
